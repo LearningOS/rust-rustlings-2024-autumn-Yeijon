05e8ad73d380f095c58f73144c99160e3aa23557
Yeijon <yeijon04@gmail.com>
2024-10-30 20:26:47 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -6,18 +6,19 @@
 // I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    // xTODO: Remove whitespace from both ends of a string!
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // xTODO: Add " world!" to the string! There's multiple ways to do this!
+    input.to_string()+ " world!"
+    // input.push_str(" world!")
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    // xTODO: Replace "cars" in the string with "balloons"!
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
