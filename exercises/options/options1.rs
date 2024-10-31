35c5f42c3baca329e8f8f5ca62ab001b2a35d8ad
Yeijon <yeijon04@gmail.com>
2024-10-31 11:20:09 +0800

    update

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -12,8 +12,14 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a
     // value of 0 The Option output should gracefully handle cases where
     // time_of_day > 23.
-    // TODO: Complete the function body - remember to return an Option!
-    ???
+    // xTODO: Complete the function body - remember to return an Option!
+    if time_of_day > 23 {
+        None
+    } else if time_of_day >= 22 {
+        Some(0)
+    } else {
+        Some(5)
+    }
 }
 
 #[cfg(test)]
@@ -31,9 +37,9 @@ mod tests {
 
     #[test]
     fn raw_value() {
-        // TODO: Fix this test. How do you get at the value contained in the
+        // xTODO: Fix this test. How do you get at the value contained in the
         // Option?
-        let icecreams = maybe_icecream(12);
+        let icecreams = maybe_icecream(12).unwrap();
         assert_eq!(icecreams, 5);
     }
 }
