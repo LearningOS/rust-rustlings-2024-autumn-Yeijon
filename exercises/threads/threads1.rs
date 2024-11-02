bf050822cc4047b4ca33eba53820e52f774477fa
Yeijon <yeijon04@gmail.com>
2024-11-02 10:16:13 +0800

    update

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -26,7 +26,8 @@ fn main() {
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        // xTODO: a struct is returned from thread::spawn, can you use it?
+        results.push(handle.join().unwrap())
     }
 
     if results.len() != 10 {
